e0824aea8d9e75d0d08ad89329023e71ed2cff37
Toddlesplash001 <118872908+Toddlesplash001@users.noreply.github.com>
2022-12-23 09:07:26 +0530

    Add files via upload

---
+++ Assignment 2 Q4.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first number "))
+b = int(input("Enter second number "))
+c = int(input("Enter third number "))
+if a>b and a>c:
+    print((a),"is greatest")
+if b>a and b>c:
+    print((b),"is greatest")
+
+if c>a and c>b:
+    print((c),"is greatest")
